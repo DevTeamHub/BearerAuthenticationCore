d789dba82f55d55414bca69ca53b58336bd4b1ab
Dmitry Bezzubenkov <dbezzubenkov@dev-team.com>
2016-07-13 10:30:26 +0300

    bearerauthenticationcore source code

---
+++ IExternalUserStore.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace DevTeam.BearerAuthenticationCore
+{
+    public interface IExternalUserStore<TUser>: IUserStore<TUser> 
+        where TUser: class, IUser
+    {
+        TUser GetExternalUser(string externalId, string externalName, string provider);
+    }
+}
